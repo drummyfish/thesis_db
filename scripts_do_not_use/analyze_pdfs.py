6c6425a46452be493d9c01a036601c21bd53ef8e
drummyfish <tastyfish@seznam.cz>
2017-06-14 18:51:40 +0200

    fit but phd theses pdf analysis

--- scripts_do_not_use/analyze_pdfs.py
+++ scripts_do_not_use/analyze_pdfs.py
@@ -7,7 +7,7 @@ sys.path.insert(0,"../tools")
 
 from theses_common import *
 
-INPUT_FILE = "../theses.json"
+INPUT_FILE = "theses_pdf_updated.json"
 OUTPUT_FILE = "theses_pdf_updated.json"
 END_AFTER = 200
 
@@ -16,15 +16,20 @@ theses = load_json(INPUT_FILE)
 def thesis_needs_pdf_analysis(thesis):
   return thesis["pages"] == None or thesis["typesetting_system"] == None or thesis["size"] == None or thesis["language"] == None
 
-for i in range(END_AFTER):
-  print(i)
+for i in range(len(theses)):
+#for i in range(END_AFTER):
+#  print(i)
 
   try:
-    random_index = random.randint(0,len(theses) - 1)
+    #random_index = random.randint(0,len(theses) - 1)
+    random_index = i
+
     random_thesis = theses[random_index]
 
-    if random_thesis["url_fulltext"] != None and thesis_needs_pdf_analysis(random_thesis):
-      print("chosen: " + thesis_to_string(random_thesis))
+    if random_thesis["kind"] == THESIS_PHD and random_thesis["faculty"] == FACULTY_FIT_BUT and random_thesis["url_fulltext"] != None and thesis_needs_pdf_analysis(random_thesis):
+
+    #if random_thesis["url_fulltext"] != None and thesis_needs_pdf_analysis(random_thesis):
+      print(i,"chosen: " + thesis_to_string(random_thesis))
       print("needs PDF update")
 
       pdf_info = download_and_analyze_pdf(random_thesis["url_fulltext"])
@@ -46,6 +51,5 @@ for i in range(END_AFTER):
   except Exception as e:
     print("ERROR: " + str(e))
 
-  if i == END_AFTER - 1:
-    print("saving...")
-    save_json(theses,OUTPUT_FILE)
+print("saving...")
+save_json(theses,OUTPUT_FILE)
